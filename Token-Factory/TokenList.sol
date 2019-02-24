a91b3af821065bd6c3d117151aa5ee174e1ae985
RemediosGonzalez <mremedios.gonzalez@gmail.com>
2019-02-24 17:36:46 +0100

    Update TokenList.sol

--- Token-Factory/TokenList.sol
+++ Token-Factory/TokenList.sol
@@ -6,17 +6,22 @@ contract TokenList is Owned{
     mapping (address => uint) tokens;
     address[] tokenList;
 
-    function addToken(address token) public onlyOwner{
-        require(!isToken(token), "Token already on list");
-        tokens[token] = tokenList.push(token) - 1;
+    function addToken(address account) public onlyOwner{
+        require(!isToken(account), "Token already on list");
+        tokens[account] = tokenList.push(account) - 1;
+        emit AddToken(account, false, block.timestamp);
     }
 
-    function isToken(address token) private view returns (bool isIndeed){
+    function isToken(address account) private view returns (bool isIndeed){
         if(tokenList.length == 0) return false;
-        return (token == tokenList[tokens[token]]);
+        return (account == tokenList[tokens[account]]);
     }
 
     function getAllTokens() public view onlyOwner returns (address[] memory){
         return tokenList;        
     }
+    
+    event AddToken(address indexed account, bool success, uint timestamp);
+  
+    
 }
