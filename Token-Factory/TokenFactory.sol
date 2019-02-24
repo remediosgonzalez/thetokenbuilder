2f1c5aac7551b5fbd7fe75cdbda847c7dc41afac
RemediosGonzalez <mremedios.gonzalez@gmail.com>
2019-02-24 15:22:52 +0100

    Create TokenFactory.sol

---
+++ Token-Factory/TokenFactory.sol
@@ -0,0 +1,55 @@
+pragma solidity >=0.5.0;
+import './TokenERC20.sol';
+import './TokenList.sol';
+
+import "./WhiteList.sol";
+
+contract TokenFactory is WhiteList,TokenList,TokenERC20{
+
+    address public owner;
+    
+    TokenList tokenList;
+    TokenERC20 tokenERC20;
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    //mapping (address=> PersonalToken) personalToken;
+    
+    
+    constructor () public {
+        owner = msg.sender;
+        //tokenList = new TokenList();
+    }
+
+    
+    function createToken(string memory name, string memory symbol, uint8 decimals, uint256 totalSupply, address account) public { //onlyExistAccount(msg.sender) onlyPay(msg.sender){
+        tokenERC20 = new TokenERC20(name, symbol, decimals, totalSupply, account);
+        tokenList.addToken(address(account));        
+        emit CreateToken(name,symbol,decimals,totalSupply,account);
+        
+    }
+    
+    /** 
+    Devuelve la lista completa de direcciones de tokens creados
+    */
+    function getAllTokens() public view onlyOwner returns (address[] memory addressList){
+        addressList = tokenList.getAllTokens();
+    }
+    
+   /* function setNewContract (address account, address newContract) internal onlyOwnerPersonal(msg.sender) returns(bool success) {
+        personalToken[account].connectorContract = newContract;
+        return true;
+    }*/
+    
+    
+    
+   /* function totalSupply() public returns (uint){
+        return supply;
+    }
+    //No aceptamos ether
+    function () public payable {
+        revert();
+    }*/
+    
+    event CreateToken(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply, address account);   
+}
