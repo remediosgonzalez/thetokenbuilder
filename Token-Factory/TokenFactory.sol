36d23a4ddfa40a843682777011a2d65ae4e35284
RemediosGonzalez <mremedios.gonzalez@gmail.com>
2019-02-24 18:36:59 +0100

    Update TokenFactory.sol

--- Token-Factory/TokenFactory.sol
+++ Token-Factory/TokenFactory.sol
@@ -1,10 +1,10 @@
 pragma solidity >=0.5.0;
 import './TokenERC20.sol';
-//import './TokenList.sol';
+import './TokenList.sol';
 
 import "./WhiteList.sol";
 
-contract TokenFactory is WhiteList{
+contract TokenFactory is WhiteList, TokenList{
 
     address public owner;
     
@@ -13,7 +13,7 @@ contract TokenFactory is WhiteList{
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
 
-    //mapping (address=> PersonalToken) personalToken;
+    mapping (address => address[]) public tokens;
     
     
     constructor () public {
@@ -24,33 +24,13 @@ contract TokenFactory is WhiteList{
     
     function createToken(string memory name, string memory symbol, uint256 decimals, uint256 totalSupply, address account) public onlyExistAccount(account) onlyPay(account){
         tokenERC20 = new TokenERC20(name, symbol, decimals, totalSupply, account);
+        //tokens[account]=address(tokenERC20)[];
         modifyPay(account,false);
-        //tokenList.addToken(address(account));        
+        addToken(address(account));        
         emit CreateToken(name,symbol,decimals,totalSupply,account);
         
     }
     
-    /** 
-    Devuelve la lista completa de direcciones de tokens creados
-    */
-    /*function getAllTokens() public view onlyOwner returns (address[] memory addressList){
-        addressList = tokenList.getAllTokens();
-    }*/
-    
-   /* function setNewContract (address account, address newContract) internal onlyOwnerPersonal(msg.sender) returns(bool success) {
-        personalToken[account].connectorContract = newContract;
-        return true;
-    }*/
-    
-    
-    
-   /* function totalSupply() public returns (uint){
-        return supply;
-    }
-    //No aceptamos ether
-    function () public payable {
-        revert();
-    }*/
     
     event CreateToken(string _name, string _symbol, uint256 _decimals, uint256 _totalSupply, address account);   
 }
