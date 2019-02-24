f86345e48094e165c82efb103400a0275d3db005
RemediosGonzalez <mremedios.gonzalez@gmail.com>
2019-02-24 15:23:00 +0100

    Create Owned.sol

---
+++ Token-Factory/Owned.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.5.0;
+
+contract Owned{
+
+    address public owner;
+
+    constructor() public{
+        owner = msg.sender;
+    }   
+
+
+   /**
+   * @dev Permite que el propietario actual pueda transmitir el control a un nuevo propietario.
+   * @param newOwner dirección del nuevo propietario.
+   */
+   function transferOwnership(address newOwner) public onlyOwner {
+      require(newOwner != address(0));
+      emit OwnershipTransferred(owner, newOwner);
+      owner = newOwner;
+    }
+
+
+    /*********************MODIFICADORES*******************************************/
+    modifier onlyOwner(){
+        require (msg.sender == owner, "only owner can call this");
+        _;
+    }
+
+   /* modifier onlyPersonalOwner(address account){
+        require(personalToken[account].owner==msg.sender);
+        _;
+    }*/
+    
+    /********************EVENTOS*******************************************/
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+
+}
